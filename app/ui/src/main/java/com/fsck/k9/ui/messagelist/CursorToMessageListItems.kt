467a36de9ae50a942f770b0a2ce0b2acae7cd61d
Jacek Kuzniarski <jacek@soverin.net>
2019-12-27 17:36:27 +0100

    Fix formatting  CursorToMessageListItems  for kLint

--- app/ui/src/main/java/com/fsck/k9/ui/messagelist/CursorToMessageListItems.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/messagelist/CursorToMessageListItems.kt
@@ -9,20 +9,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class CursorToMessageListItems(
-        private val preferences: Preferences,
-        private val helper: MessageHelper,
-        private val resources: Resources,
-        private val uniqueColumnId: Int
+    private val preferences: Preferences,
+    private val helper: MessageHelper,
+    private val resources: Resources,
+    private val uniqueColumnId: Int
 ) {
 
     private fun messageListItemExtractor(cursor: Cursor) = MessageListItemExtractor(
-            preferences,
-            cursor,
-            helper,
-            resources
+        preferences,
+        cursor,
+        helper,
+        resources
     )
 
     fun convert(cursor: Cursor, onItemsReady: ConvertedMessagesReady) {
@@ -41,25 +40,25 @@ class CursorToMessageListItems(
 
     private inline val MessageListItemExtractor.asItem: MessageListItem
         get() = MessageListItem(
-                this.id,
-                this.uid,
-                this.folderServerId.orEmpty(),
-                this.displayName.toString(),
-                this.subject(this.threadCount),
-                this.date,
-                this.sigil,
-                this.threadCount,
-                this.threadRootId,
-                this.read,
-                this.answered,
-                this.forwarded,
-                this.chipColor,
-                this.flagged,
-                this.hasAttachments,
-                this.preview,
-                this.counterPartyAddresses,
-                this.account,
-                this.selectionIdentifier(uniqueColumnId)
+            this.id,
+            this.uid,
+            this.folderServerId.orEmpty(),
+            this.displayName.toString(),
+            this.subject(this.threadCount),
+            this.date,
+            this.sigil,
+            this.threadCount,
+            this.threadRootId,
+            this.read,
+            this.answered,
+            this.forwarded,
+            this.chipColor,
+            this.flagged,
+            this.hasAttachments,
+            this.preview,
+            this.counterPartyAddresses,
+            this.account,
+            this.selectionIdentifier(uniqueColumnId)
 
         )
 }
